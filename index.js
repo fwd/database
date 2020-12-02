4a544ae3dc055f19be4849c5e34b4e00438bff06
Esteban Santiesteban <me@estebansanti.com>
2020-12-01 22:23:52 -0500

    add cloud plugin

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
-const firebase = require('firebase/app');
-require('firebase/database');
-
 module.exports = (plugin, config) => {
 
 	plugin = plugin.toLowerCase()
 	plugin = plugin || ''
+
+	if (plugin === 'cloud') {
+		return require('./plugin/cloud')(config)
+	}
 
 	if (plugin === 'local') {
 		return require('./plugin/local')(config)
 	}
 
 	if (plugin === 'firebase') {
+		const firebase = require('firebase/app');
+			  require('firebase/database');
 		return require('./plugin/firebase')(config)
 	}
 
---
+++ plugins/cloud.js
@@ -0,0 +1,44 @@
+const server = require('@fwd/server')
+
+const base_url = 'https://data.forward.miami'
+
+module.exports = (config) => {
+
+	return {
+		get(key, query, database) {
+			return this.find(key, query, database)
+		},
+		find(key, query, database) {
+			return new Promise(async (resolve, reject) => {
+				var qs = Object.keys(query || {}).map(key => `${key}=${params[key]}`).join('&');
+				var response = await server.http.get(`${base_url}/${key}?apiKey=${config.apikey}${qs}`)
+				resolve(response.data.response)
+			})
+		},
+		create(key, value, database) {
+			return new Promise(async (resolve, reject) => {
+				var response = await server.http.post(`${base_url}/${key}?apiKey=${config.apikey}`, value)
+				resolve( response.data.response )
+			})
+		},
+		update(key, id, update, database) {
+			return new Promise(async (resolve, reject) => {
+				var response = await server.http.post(`${base_url}/${key}/${id}?apiKey=${config.apikey}`, body)
+				resolve( response.data.response )
+			})
+		},
+		set(key, database) {
+			return new Promise(async (resolve, reject) => {
+				var response = await server.http.put(`${base_url}/${key}?apiKey=${config.apikey}`,)
+				resolve( response.data.response )
+			})
+		},
+		remove(key, database) {
+			return new Promise(async (resolve, reject) => {
+				var response = await server.http.delete(`${base_url}/${path}?apiKey=${config.apikey}`,)
+				resolve( response.data.response )
+			})
+		},
+	}
+
+}
