633fa62bcf5317d5d01cb445b6269a009fad9ea3
Esteban Santiesteban <me@estebansanti.com>
2021-04-05 13:49:36 -0400

    fix local database bug

--- plugins/local2.js
+++ plugins/local2.js
@@ -248,8 +248,7 @@ module.exports = (config) => {
 				}
 
 				if (query && query.id && await check(`${key}/${query.id}`) && fs.lstatSync(`${key}/${query.id}`).isFile() ) {
-					console.log("found it faster!")
-					resolve(read(`${key}/${query.id}`))
+					resolve([read(`${key}/${query.id}`)])
 					return
 				}
 
