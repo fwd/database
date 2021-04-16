1429df285b635324ac1477cd60be41797d5e357f
Esteban <me@estebansanti.com>
2021-04-15 21:39:19 -0400

    Update cloud.js

--- plugins/cloud.js
+++ plugins/cloud.js
@@ -18,6 +18,15 @@ module.exports = (config) => {
     }
 
     const http = server.http.create(headers)
+
+    function namespace(model) {
+        model = model === '/' ? '' : model
+        if (model.includes('/')) {
+            return `/${model}`
+        } else {
+            return `/default/${model}`
+        }
+    }
     
     return {
         get(key, query) {
@@ -39,31 +48,31 @@ module.exports = (config) => {
         find(key, query) {
             return new Promise(async (resolve, reject) => {
                 var qs = Object.keys(query || {}).map(key => `${key}=${query[key]}`).join('&');
-                var response = await http.get(`/${key === '/' ? '' : key}${ qs ? '?' + qs : '' }`)
+                var response = await http.get(`${namespace(key)}${ qs ? '?' + qs : '' }`)
                 resolve(response.data.response)
             })
         },
         create(key, value) {
             return new Promise(async (resolve, reject) => {
-                var response = await http.post(`/${key === '/' ? '' : key}`, value)
+                var response = await http.post(namespace(key), value)
                 resolve(response.data.response)
             })
         },
         update(key, id, update) {
             return new Promise(async (resolve, reject) => {
-                var response = await http.post(`/${key}/${id}`, update)
+                var response = await http.post(`${namespace(key)}/${id}`, update)
                 resolve(response.data.response)
             })
         },
         set(key, update) {
             return new Promise(async (resolve, reject) => {
-                var response = await http.put(`/${key}`, update)
+                var response = await http.put(namespace(key), update)
                 resolve(response.data.response)
             })
         },
         remove(key, id) {
             return new Promise(async (resolve, reject) => {
-                var response = await http.delete(`/${key}/${id}`)
+                var response = await http.delete(`${namespace(key)}/${id}`)
                 resolve(response.data.response)
             })
         },
