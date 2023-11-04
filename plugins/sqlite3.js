a2a773a068719f31b930f3af48ab96be0dc3ca71
Esteban <esteban@fwd.dev>
2023-11-03 22:20:52 -0400

    Update sqlite3.js

--- plugins/sqlite3.js
+++ plugins/sqlite3.js
@@ -1,23 +1,87 @@
+const sqlite3 = require('sqlite3').verbose();
 
 module.exports = (config) => {
+    
+    const base_path = config.filepath || config.base_path || config.basepath;
 
-    const base_path = config.filepath || config.base_path || config.basepath
-    const database_name = config.database || config.filename
+    const db = new sqlite3.Database(base_path || ':memory:');
 
-    // TODO
-    // Be a hero, send a pull request
+    // Create a table for the given model
+    const createTable = (model) => {
+        return new Promise((resolve, reject) => {
+            const createTableQuery = `CREATE TABLE IF NOT EXISTS ${model} (id INTEGER PRIMARY KEY, data TEXT)`;
+            db.run(createTableQuery, (err) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve();
+                }
+            });
+        });
+    };
 
     return {
-        list(model) {},
-        get(model, query) {},
-        findFirst(model, query) {},
-        findLast(model, query) {},
-        findOne(model, query) {},
-        paginate(model, query) {},
-        find(model, query) {},
-        create(model, value) {},
-        update(model, id, update) {},
-        set(model, value) {},
-        remove(model, id) {},
-    }
-}
+        list: (model) => {
+            return new Promise((resolve, reject) => {
+                const query = `SELECT * FROM ${model}`;
+                db.all(query, (err, rows) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve(rows);
+                    }
+                });
+            });
+        },
+        get: (model, id) => {
+            return new Promise((resolve, reject) => {
+                const query = `SELECT * FROM ${model} WHERE id = ?`;
+                db.get(query, [id], (err, row) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve(row);
+                    }
+                });
+            });
+        },
+        create: (model, value) => {
+            return createTable(model).then(() => {
+                return new Promise((resolve, reject) => {
+                    const insertQuery = `INSERT INTO ${model} (data) VALUES (?)`;
+                    db.run(insertQuery, [JSON.stringify(value)], function (err) {
+                        if (err) {
+                            reject(err);
+                        } else {
+                            resolve({ id: this.lastID });
+                        }
+                    });
+                });
+            });
+        },
+        update: (model, id, update) => {
+            return new Promise((resolve, reject) => {
+                const updateQuery = `UPDATE ${model} SET data = ? WHERE id = ?`;
+                db.run(updateQuery, [JSON.stringify(update), id], function (err) {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve({ changes: this.changes });
+                    }
+                });
+            });
+        },
+        remove: (model, id) => {
+            return new Promise((resolve, reject) => {
+                const deleteQuery = `DELETE FROM ${model} WHERE id = ?`;
+                db.run(deleteQuery, [id], function (err) {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve({ changes: this.changes });
+                    }
+                });
+            });
+        },
+    };
+};
