896cd98cf16a1d35a722db4a950377930bd3e31b
Esteban <esteban@fwd.dev>
2023-11-03 22:25:27 -0400

    Update mysql.js

--- plugins/mysql.js
+++ plugins/mysql.js
@@ -1,23 +1,125 @@
+const mysql = require('mysql');
 
 module.exports = (config) => {
 
-    const base_path = config.filepath || config.base_path || config.basepath
-    const database_name = config.database || config.filename
-
-    // TODO
-    // Be a hero, send a pull request
+    // Create a MySQL connection pool using the provided configuration
+    const pool = mysql.createPool(config);
 
     return {
-        list(model) {},
-        get(model, query) {},
-        findFirst(model, query) {},
-        findLast(model, query) {},
-        findOne(model, query) {},
-        paginate(model, query) {},
-        find(model, query) {},
-        create(model, value) {},
-        update(model, id, update) {},
-        set(model, value) {},
-        remove(model, id) {},
-    }
-}
+        list(model) {
+            return new Promise((resolve, reject) => {
+                pool.query(`SELECT * FROM ${model}`, (error, results) => {
+                    if (error) {
+                        reject(error);
+                    } else {
+                        resolve(results);
+                    }
+                });
+            });
+        },
+
+        get(model, id) {
+            return new Promise((resolve, reject) => {
+                pool.query(`SELECT * FROM ${model} WHERE id = ?`, [id], (error, results) => {
+                    if (error) {
+                        reject(error);
+                    } else {
+                        resolve(results[0]);
+                    }
+                });
+            });
+        },
+
+        findFirst(model, query) {
+            return new Promise((resolve, reject) => {
+                pool.query(`SELECT * FROM ${model} WHERE ? LIMIT 1`, query, (error, results) => {
+                    if (error) {
+                        reject(error);
+                    } else {
+                        resolve(results[0]);
+                    }
+                });
+            });
+        },
+
+        findLast(model, query) {
+            return new Promise((resolve, reject) => {
+                pool.query(`SELECT * FROM ${model} WHERE ? ORDER BY id DESC LIMIT 1`, query, (error, results) => {
+                    if (error) {
+                        reject(error);
+                    } else {
+                        resolve(results[0]);
+                    }
+                });
+            });
+        },
+
+        findOne(model, query) {
+            return new Promise((resolve, reject) => {
+                pool.query(`SELECT * FROM ${model} WHERE ? LIMIT 1`, query, (error, results) => {
+                    if (error) {
+                        reject(error);
+                    } else {
+                        resolve(results[0]);
+                    }
+                });
+            });
+        },
+
+        paginate(model, query) {
+            // Implement pagination logic using query parameters
+        },
+
+        find(model, query) {
+            return new Promise((resolve, reject) => {
+                pool.query(`SELECT * FROM ${model} WHERE ?`, query, (error, results) => {
+                    if (error) {
+                        reject(error);
+                    } else {
+                        resolve(results);
+                    }
+                });
+            });
+        },
+
+        create(model, value) {
+            return new Promise((resolve, reject) => {
+                pool.query(`INSERT INTO ${model} SET ?`, value, (error, results) => {
+                    if (error) {
+                        reject(error);
+                    } else {
+                        resolve(results.insertId);
+                    }
+                });
+            });
+        },
+
+        update(model, id, update) {
+            return new Promise((resolve, reject) => {
+                pool.query(`UPDATE ${model} SET ? WHERE id = ?`, [update, id], (error, results) => {
+                    if (error) {
+                        reject(error);
+                    } else {
+                        resolve(results.affectedRows > 0);
+                    }
+                });
+            });
+        },
+
+        set(model, value) {
+            // Implement your custom logic to set data in the model
+        },
+
+        remove(model, id) {
+            return new Promise((resolve, reject) => {
+                pool.query(`DELETE FROM ${model} WHERE id = ?`, [id], (error, results) => {
+                    if (error) {
+                        reject(error);
+                    } else {
+                        resolve(results.affectedRows > 0);
+                    }
+                });
+            });
+        },
+    };
+};
