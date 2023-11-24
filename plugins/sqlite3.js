eb8569fe18004067c0682c7b4eaac30267b95d61
Esteban <esteban@fwd.dev>
2023-11-24 14:30:22 -0500

    Update sqlite3.js

--- plugins/sqlite3.js
+++ plugins/sqlite3.js
@@ -38,9 +38,9 @@ module.exports = (config) => {
 
     return {
         
-        raw(query) {
+        raw(query, params) {
             return new Promise((resolve, reject) => {
-                db.all(query, (err, rows) => {
+                db.all(query, params, (err, rows) => {
                     if (err) {
                         reject(err);
                     } else {
